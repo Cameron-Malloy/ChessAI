2c40faa51f4140210afa035857a3278a1a8e091f
Cameron-Malloy <cammalloy@outlook.com>
2024-03-03 16:59:48 -0800

    Added a lot of new things such as a start and win screen. Also, a restart button and a point tracker.

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -12,54 +12,59 @@ import java.util.LinkedList;
 public class ChessBoard extends JPanel {
     public static LinkedList<Pieces> ps = new LinkedList<>();
     public static Pieces selectedPiece=null;
+    public static int size = 32;
 
-    public static void main(String[] args) throws IOException {
-        BufferedImage all = ImageIO.read(new File("C:\\Users\\camma\\Downloads\\chess.png"));
-        Image imgs[] = new Image[12];
+    public static void main(String[] args) throws IOException, AWTException {
+        run();
+        }
+    public static void run() throws AWTException, IOException {
+        JFrame jf = new JFrame();
+            Robot bot = new Robot();
         int ind = 0;
+        BufferedImage all = ImageIO.read(new File("C:\\Users\\camma\\Downloads\\chess.png"));
+        Image[] imgs = new Image[12];
+
         for (int y = 0; y < 400; y += 200) {
             for (int x = 0; x < 1200; x += 200) {
                 imgs[ind] = all.getSubimage(x, y, 200, 200).getScaledInstance(64, 64, BufferedImage.SCALE_SMOOTH);
                 ind++;
             }
         }
+        Pieces bRook = new Pieces(0, 0, false, "rook",5, ps);
+        Pieces bKnight = new Pieces(1, 0, false, "knight",3, ps);
+        Pieces bBishop = new Pieces(2, 0, false, "bishop",3, ps);
+        Pieces bQueen = new Pieces(3, 0, false, "queen",9, ps);
+        Pieces bKing = new Pieces(4, 0, false, "king",0,  ps);
+        Pieces bBishop2 = new Pieces(5, 0, false, "bishop", 3, ps);
+        Pieces bKnight2 = new Pieces(6, 0, false, "knight", 3, ps);
+        Pieces bRook2 = new Pieces(7, 0, false, "rook",5, ps);
+        Pieces bPawn1 = new Pieces(1, 1, false, "pawn",1, ps);
+        Pieces bPawn2 = new Pieces(2, 1, false, "pawn",1, ps);
+        Pieces bPawn3 = new Pieces(3, 1, false, "pawn", 1, ps);
+        Pieces bPawn4 = new Pieces(4, 1, false, "pawn",1, ps);
+        Pieces bPawn5 = new Pieces(5, 1, false, "pawn",1, ps);
+        Pieces bPawn6 = new Pieces(6, 1, false, "pawn",1, ps);
+        Pieces bPawn7 = new Pieces(7, 1, false, "pawn",1, ps);
+        Pieces bPawn8 = new Pieces(0, 1, false, "pawn",1, ps);
 
-        Pieces bRook = new Pieces(0, 0, false, "rook", ps);
-        Pieces bKnight = new Pieces(1, 0, false, "knight", ps);
-        Pieces bBishop = new Pieces(2, 0, false, "bishop", ps);
-        Pieces bQueen = new Pieces(3, 0, false, "queen", ps);
-        Pieces bKing = new Pieces(4, 0, false, "king", ps);
-        Pieces bBishop2 = new Pieces(5, 0, false, "bishop", ps);
-        Pieces bKnight2 = new Pieces(6, 0, false, "knight", ps);
-        Pieces bRook2 = new Pieces(7, 0, false, "rook", ps);
-        Pieces bPawn1 = new Pieces(1, 1, false, "pawn", ps);
-        Pieces bPawn2 = new Pieces(2, 1, false, "pawn", ps);
-        Pieces bPawn3 = new Pieces(3, 1, false, "pawn", ps);
-        Pieces bPawn4 = new Pieces(4, 1, false, "pawn", ps);
-        Pieces bPawn5 = new Pieces(5, 1, false, "pawn", ps);
-        Pieces bPawn6 = new Pieces(6, 1, false, "pawn", ps);
-        Pieces bPawn7 = new Pieces(7, 1, false, "pawn", ps);
-        Pieces bPawn8 = new Pieces(0, 1, false, "pawn", ps);
-
-        Pieces wRook = new Pieces(0, 7, true, "rook", ps);
-        Pieces wKnight = new Pieces(1, 7, true, "knight", ps);
-        Pieces wBishop = new Pieces(2, 7, true, "bishop", ps);
-        Pieces wQueen = new Pieces(3, 7, true, "queen", ps);
-        Pieces wKing = new Pieces(4, 7, true, "king", ps);
-        Pieces wBishop2 = new Pieces(5, 7, true, "bishop", ps);
-        Pieces wKnight2 = new Pieces(6, 7, true, "knight", ps);
-        Pieces wRook2 = new Pieces(7, 7, true, "rook", ps);
-        Pieces wPawn1 = new Pieces(1, 6, true, "pawn", ps);
-        Pieces wPawn2 = new Pieces(2, 6, true, "pawn", ps);
-        Pieces wPawn3 = new Pieces(3, 6, true, "pawn", ps);
-        Pieces wPawn4 = new Pieces(4, 6, true, "pawn", ps);
-        Pieces wPawn5 = new Pieces(5, 6, true, "pawn", ps);
-        Pieces wPawn6 = new Pieces(6, 6, true, "pawn", ps);
-        Pieces wPawn7 = new Pieces(7, 6, true, "pawn", ps);
-        Pieces wPawn8 = new Pieces(0, 6, true, "pawn", ps);
+        Pieces wRook = new Pieces(0, 7, true, "rook",5, ps);
+        Pieces wKnight = new Pieces(1, 7, true, "knight",3, ps);
+        Pieces wBishop = new Pieces(2, 7, true, "bishop",3, ps);
+        Pieces wQueen = new Pieces(3, 7, true, "queen",9, ps);
+        Pieces wKing = new Pieces(4, 7, true, "king",0, ps);
+        Pieces wBishop2 = new Pieces(5, 7, true, "bishop",3, ps);
+        Pieces wKnight2 = new Pieces(6, 7, true, "knight",3, ps);
+        Pieces wRook2 = new Pieces(7, 7, true, "rook",5, ps);
+        Pieces wPawn1 = new Pieces(1, 6, true, "pawn",1, ps);
+        Pieces wPawn2 = new Pieces(2, 6, true, "pawn",1, ps);
+        Pieces wPawn3 = new Pieces(3, 6, true, "pawn",1, ps);
+        Pieces wPawn4 = new Pieces(4, 6, true, "pawn",1, ps);
+        Pieces wPawn5 = new Pieces(5, 6, true, "pawn",1, ps);
+        Pieces wPawn6 = new Pieces(6, 6, true, "pawn",1, ps);
+        Pieces wPawn7 = new Pieces(7, 6, true, "pawn",1, ps);
+        Pieces wPawn8 = new Pieces(0, 6, true, "pawn",1, ps);
 
 
-        JFrame jf = new JFrame();
         jf.setBounds(10, 10, 512, 512);
         jf.setUndecorated(true);
         jf.setTitle("The Best Chess Board");
@@ -67,7 +72,40 @@ public class ChessBoard extends JPanel {
         jf.setLocationRelativeTo(null);
         jf.setBackground(Color.GRAY);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        JFrame start = new JFrame();
+
+
+        JLabel label = new JLabel("Start Game Here  (Press alt + F4 to Quit Game)", JLabel.CENTER);
+        label.setAlignmentX(0);
+        label.setAlignmentY(0);
+        label.setBounds(100, 100, 300, 100);
+
+        JButton button = new JButton();
+        button.setAlignmentX(0);
+        button.setAlignmentY(0);
+        button.setBounds(125, 200, 250, 100);
+        button.setText("Start");
+        button.setFocusable(false);
+        button.setBorder(BorderFactory.createBevelBorder(5));
+        button.setForeground(Color.cyan);
+        button.setBackground(Color.darkGray);
+
+
+        start.setBounds(100, 100, 512, 512);
+        start.add(label);
+        start.add(button);
+        start.setLayout(null);
+        start.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        start.setLocationRelativeTo(null);
+        start.setVisible(true);
+        button.addActionListener(e -> jf.setVisible(true));
+        button.addActionListener(e -> trackerScreen());
+
+
         JPanel pn = new JPanel() {
+
             @Override
             public void paint(Graphics g) {
                 super.paint(g);
@@ -75,9 +113,9 @@ public class ChessBoard extends JPanel {
                 for (int y = 0; y < 8; y++) {
                     for (int x = 0; x < 8; x++) {
                         if (white) {
-                            g.setColor(new Color(235, 235, 208));
+                            g.setColor(new Color(111, 111, 111));
                         } else {
-                            g.setColor(new Color(119, 148, 85));
+                            g.setColor(new Color(222, 222, 222));
                         }
                         g.fillRect(x * 64, y * 64, 64, 64);
                         white = !white;
@@ -86,9 +124,6 @@ public class ChessBoard extends JPanel {
                 }
                 for (Pieces p : ps) {
                     int ind = 0;
-                    if (p.name.equalsIgnoreCase("king")) {
-                        ind = 0;
-                    }
                     if (p.name.equalsIgnoreCase("queen")) {
                         ind = 1;
                     }
@@ -114,6 +149,7 @@ public class ChessBoard extends JPanel {
 
         };
         jf.add(pn);
+
         jf.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -124,32 +160,38 @@ public class ChessBoard extends JPanel {
             public void mousePressed(MouseEvent e) {
                 //System.out.println((getPiece(e.getX(), e.getY()).isWhite?"white":"black")+getPiece(e.getX(), e.getY()).name);
                 selectedPiece = getPiece(e.getX(), e.getY());
+                if (selectedPiece != null && Pieces.color != selectedPiece.isWhite) {
+                    bot.mouseMove(770, 400);
+                }
             }
+
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                selectedPiece.move(e.getX()/64, e.getY()/64);
-                jf.repaint();
+                if (selectedPiece != null && Pieces.color == selectedPiece.isWhite) {
+                    selectedPiece.move(e.getX() / 64, e.getY() / 64);
+                    jf.repaint();
+                }
+
             }
 
             @Override
             public void mouseEntered(MouseEvent e) {
-
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-
+                bot.mouseMove(770, 400);
             }
+
         });
         jf.addMouseMotionListener(new MouseMotionListener() {
             @Override
             public void mouseDragged(MouseEvent e) {
-                if(selectedPiece!=null){
-                    selectedPiece.x=e.getX()-32;
-                    selectedPiece.y=e.getY()-32;
+                if (selectedPiece != null && Pieces.color == selectedPiece.isWhite) {
+                    selectedPiece.x = e.getX() - 32;
+                    selectedPiece.y = e.getY() - 32;
                     jf.repaint();
-
 
                 }
             }
@@ -158,11 +200,41 @@ public class ChessBoard extends JPanel {
             public void mouseMoved(MouseEvent e) {
 
             }
-        });
-        jf.setVisible(true);
 
+        });
     }
-    public static Pieces getPiece(int x, int y){
+
+    public static void trackerScreen(){
+        JLabel score1 = new JLabel("Black's Score: " + Pieces.blackScore, JLabel.CENTER);
+        score1.setAlignmentX(0);
+        score1.setAlignmentY(0);
+        score1.setForeground(Color.white);
+        score1.setBackground(Color.white);
+
+        JFrame tracker1 = new JFrame();
+        tracker1.setBounds(512,106,513,40);
+        tracker1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tracker1.setUndecorated(true);
+        tracker1.getContentPane().setBackground(Color.black);
+        tracker1.add(score1);
+        tracker1.setVisible(true);
+        tracker1.setLayout(null);
+
+        JLabel score2 = new JLabel("White's Score: " + Pieces.whiteScore, JLabel.CENTER);
+        score2.setAlignmentX(0);
+        score2.setAlignmentY(0);
+        score2.setForeground(Color.white);
+        score2.setBackground(Color.white);
+
+        JFrame tracker2 = new JFrame();
+        tracker2.setBounds(511,658,514,40);
+        tracker2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tracker2.setUndecorated(true);
+        tracker2.getContentPane().setBackground(Color.black);
+        tracker2.setVisible(true);
+        tracker2.add(score2);
+    }
+    public static Pieces getPiece(int x, int y){//this returns the piece at a given x,y
         int xp = x/64;
         int yp = y/64;
         for(Pieces p : ps){
@@ -172,7 +244,24 @@ public class ChessBoard extends JPanel {
         }
         return null;
     }
+    public static String getName(int x, int y){//this returns the piece name
+        int xp = x/64;
+        int yp = y/64;
+        for(Pieces p : ps){
+            if(p.xp == xp&&p.yp==yp){
+                return p.name;
+            }
+        }
+        return null;
     }
+    public static void removeList(){//this returns the piece name
+        for(int i = 0; i < size; i++){
+            ps.remove();
+            }
+        }
+}
+
+
 /* another old thing that I changed
     public void paint(Graphics gp) {
         boolean white = true;
--- src/Pieces.java
+++ src/Pieces.java
@@ -1,16 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Objects;
 
-public class Pieces {
+public class Pieces extends ChessBoard {
     int xp;
     int yp;
     int x;
     int y;
-;
+
+    public static boolean color;
+
     boolean isWhite;
     LinkedList<Pieces> ps;
     String name;
+    JFrame winScreen;
+    JButton restart;
+    JLabel winText;
+    String winColor;
+    int pointsWorth;
+    public static int blackScore;
+    public static int whiteScore;
 
-    public Pieces(int xp, int yp, boolean isWhite, String n, LinkedList<Pieces> ps) {
+
+
+    public Pieces(int xp, int yp, boolean isWhite, String n, int points, LinkedList<Pieces> ps) {
         this.xp = xp;
         this.yp = yp;
         x=xp*64;
@@ -19,25 +34,98 @@ public class Pieces {
         this.ps = ps;
         name = n;
         ps.add(this);
+        color = true;
+        winScreen = new JFrame();
+        restart = new JButton();
+        winText = new JLabel();
+        winColor = new String();
+        pointsWorth = points;
+        blackScore = 0;
+        whiteScore= 0;
     }
     public void move(int xp, int yp){
-        if(ChessBoard.getPiece(xp*64, yp*64)!=null){
-            if(ChessBoard.getPiece(xp*64, yp*64).isWhite!=isWhite){
-                ChessBoard.getPiece(xp*64, yp*64).kill();
-
-            }else{
-                x=this.xp*64;
-                y=this.yp*64;
-                return;
+        if(Objects.equals(ChessBoard.getName(xp * 64, yp * 64), "king")){
+            if(color){
+                winColor = "White";
             }
+            if(!color){
+                winColor = "Black";
+            }
+                winScreen().setVisible(true);
         }
+        if (ChessBoard.getPiece(xp * 64, yp * 64) != null) {//is there something in the spot moving to
+                if (ChessBoard.getPiece(xp * 64, yp * 64).isWhite != isWhite) {//is the piece moving onto different color piece?
+                    ChessBoard.getPiece(xp * 64, yp * 64).kill();//if so kill
+
+                } else {
+                    x = this.xp * 64;
+                    y = this.yp * 64;
+
+                    return;
+                }
+            }
+
+
+        lastColor();
 
         this.xp=xp;
         this.yp=yp;
         x=xp*64;
         y=yp*64;
-        }
+
+    }
         public void kill(){
-        ps.remove(this);
+            if(ChessBoard.getPiece(xp * 64, yp * 64).isWhite == true){
+                blackScore += Pieces.getPiece(xp * 64, yp * 64).pointsWorth;
+                trackerScreen();
+            }else
+                whiteScore += Pieces.getPiece(xp * 64, yp * 64).pointsWorth;
+                trackerScreen();
+            size = size-1;
+            ps.remove(this);
+        }
+        //Find the color of last piece moved
+        //White is true, Black is false
+        public void lastColor(){
+            color = !color;
+        }
+        public JFrame winScreen() {
+            removeList();
+            size = 32;
+            winScreen.setBounds(440, 65, 700, 700);
+            restart.setAlignmentX(0);
+            restart.setAlignmentY(0);
+            restart.setBounds(220, 275, 250, 100);
+            restart.setText("Press to restart");
+            restart.setFocusable(false);
+            restart.setBorder(BorderFactory.createBevelBorder(5));
+            restart.setForeground(Color.cyan);
+            restart.setBackground(Color.darkGray);
+            winScreen.add(restart);
+
+            if(Objects.equals(winColor, "Black")){
+                winText.setText(winColor + " Wins With " + blackScore +" Points!");
+            }else winText.setText(winColor + " Wins With " + whiteScore +" Points!");
+
+
+            winText.setAlignmentX(0);
+            winText.setAlignmentY(0);
+            winText.setBounds(275,200, 300, 100);
+            winScreen.add(winText);
+
+            winScreen.setLayout(null);
+            winScreen.getContentPane().setBackground(Color.lightGray);
+            winScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            winScreen.setLocationRelativeTo(null);
+            winScreen.setVisible(true);
+            restart.addActionListener(e -> winScreen.setVisible(false));
+            restart.addActionListener(e -> {
+                try {
+                    run();
+                } catch (AWTException | IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+            return winScreen;
         }
 }
